ac5d6362997eda335ec69a12be5162c6b44074f2
Black-tag <unnianandunni007@gmail.com>
2025-10-11 14:12:40 +0000

    added broadcaster for copying msg to every subscriber channel

--- main.go
+++ main.go
@@ -10,21 +10,37 @@ import (
 
 
 func publisher (wg *sync.WaitGroup, msgChan chan string) {
+	defer wg.Done()
 	for i:= range 10 {
 		msgChan <- fmt.Sprintf("message %d", i)
 	}
 	close(msgChan)
-	wg.Done()
+	
 }
 
 
 
-func subscriber (id int, wg *sync.WaitGroup, msgChan chan string) {
-	for message := range msgChan {
+func subscriber (id int, wg *sync.WaitGroup, subChan chan string) {
+	defer wg.Done()
+	for message := range subChan {
 		fmt.Printf("subscriber %d recieved message: %s\n", id, message)
 	}
-	wg.Done()
+	
 
+}
+
+
+func broadCaster(wg *sync.WaitGroup, msgChan chan string, subscribers []chan string) {
+	defer wg.Done()
+	for msg := range msgChan {
+		for _, subChan := range subscribers {
+			subChan <- msg
+		}
+	}
+	for _, subChan := range subscribers {
+		close(subChan)
+		
+	}
 }
 
 
@@ -32,16 +48,24 @@ func subscriber (id int, wg *sync.WaitGroup, msgChan chan string) {
 
 func main() {
 
-	var msgChan = make(chan string)
+	msgChan := make(chan string)
+	subscribers := make([]chan string, 3)
+
+	for i:= range 3 {
+		subscribers[i] = make(chan string)
+	}
 
 	wg := &sync.WaitGroup{}
 
 	wg.Add(1)
 	go publisher(wg, msgChan)
+
+	wg.Add(1)
+	go broadCaster(wg, msgChan, subscribers)
 	for i:= range 3 {
+
 		wg.Add(1)
-		
-		go subscriber(i, wg, msgChan)
+		go subscriber(i, wg, subscribers[i])
 	}
 	wg.Wait()
 
